b66d89541b609188872843db6b2d7efe2a4baecc
vngson <Soncuhkb1111@gmail.com>
2024-07-21 15:54:49 +0700

    refactor pages

--- src/pages/StoryDetail/StoryDetail.tsx
+++ src/pages/StoryDetail/StoryDetail.tsx
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+// import { useState} from 'react';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark, faBookOpen, faCaretRight, faCommentDots, faListUl, faStar } from "@fortawesome/free-solid-svg-icons";
